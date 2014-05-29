08917c29898892b5712ce5605da12a06f1c828a7
Trevor Sargent <tbird1710@gmail.com>
2014-05-28 23:03:41 -0700

    started working on game structure

--- js/carnival.js
+++ js/carnival.js
@@ -1,19 +1,85 @@
-function out(line){
+function cout(line){
 	$("<p>"+line+"<p>").insertBefore("#placeholder");
 }
 
+function line(){
+	$("<p></br></p>").insertBefore("#placeholder");
+}
+
+function welcome(){
+	line(); line(); line(); line();line(); line(); line(); line();
+	cout('-----------------------------')
+	cout('| welcome to the carnival!! |');
+	cout('-----------------------------')
+	line();
+}
+
+function Person(){
+	name;
+	age;
+	height;
+	money;
+	currentLocation;
+
+	function walkTo (aPlace){
+		this.currentLocation = aPlace.name;
+	}
+
+}
+
+function Place(){
+	name;
+	surroundingsgo;
+	objects;
+	lookAroundText;
+
+	function describe(){
+		if(this.surroundings["left"] != )
+	}
+	
+}
+
+function setUp(){
+	parkingLot = new Place();
+	ticketEntrance = new Place();
+	mainSquare = new Place();
+	ferrisWheel = new Place();
+
+	parkingLot.name = "the parking lot";
+	parkingLot.surroundings
+}
+
 $(document).ready(function() {	
-	out('welcome to the carnival')
+
+
+	welcome();
+
+	setUp();
+
+	player1 = new Person();
+
+
 	$("form").submit(function() {
 		var input = $("#command_line").val();
+		cout(">> "+input);
+		line();
 		
 		switch (input){
 			case 'help':
-			out('helpful commands:');
-			out('look around: returns a description of your surroundings');
+				cout(' ');
+				cout('helpful commands:');
+				cout('- look around');
+				cout('(returns a description of your surroundings)');
+				cout('- pockets');
+				cout('(returns a list of everything you have in your pockets)');
+				cout('- walk to [place]');
+				cout('- give [item] to [person]');
+				cout('- take [item] from [person]');
+			break;
 		}
 		
-		$("#console").scrollTop($("#console")[0].scrollHeight);
+		$("html, body").animate({ scrollTop: $(document).height() }, 500);
+		line();
 		$("#command_line").val("");
 	});
 });
