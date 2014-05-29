95e95015bc0b70e38a323fbd1768ca95707994c6
Trevor Sargent <tbird1710@gmail.com>
2014-05-28 21:32:55 -0700

    basic funcitonallity of console working

--- js/carnival.js
+++ js/carnival.js
@@ -1,12 +1,19 @@
-$(document).ready(function() {
+function out(line){
+	$("<p>"+line+"<p>").insertBefore("#placeholder");
+}
 
+$(document).ready(function() {	
+	out('welcome to the carnival')
 	$("form").submit(function() {
 		var input = $("#command_line").val();
 		
-		$('<p>'+input+'</p>').insertBefore('#placeholder');
+		switch (input){
+			case 'help':
+			out('helpful commands:');
+			out('look around: returns a description of your surroundings');
+		}
 		
 		$("#console").scrollTop($("#console")[0].scrollHeight);
-		
 		$("#command_line").val("");
 	});
 });
